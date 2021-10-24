4ce7fc00623acc810dfd0248a50156d882f0a6c5
daniel <yoojeenkim@gmail.com>
2021-10-24 16:14:43 -0400

    created routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+const homepageRoutes = require('./homepage-routes');
+const dashboardRoutes = require('./dashboard-routes');
 const apiRoutes = require('./api');
 
+router.use('/', homepageRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
